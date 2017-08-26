6e1f744846bd6ac77ecfd431695552357a7f522c
Yusuke Otawa <otawa.yusuke@gmail.com>
2017-08-26 17:07:31 +0900

    Add permitted parameters for user models

---
+++ app/controllers/registrations_modified_controller.rb
@@ -0,0 +1,9 @@
+class RegistrationsModifiedController < DeviseTokenAuth::RegistrationsController
+  private
+  def sign_up_params
+    params.permit(:name, :email, :password)
+  end
+  def account_update_params
+    params.permit(:name, :email)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
+  mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        registrations: 'registrations_modified'
+  }
+  get 'current_user' => 'users#current_user'
   root 'todos#index'
   resources :todos
   resources :projects
