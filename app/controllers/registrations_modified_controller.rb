c6fb8a77a9dafa86a9346e542b46ed7b3e84de85
Yusuke Otawa <otawa.yusuke@gmail.com>
2017-09-04 12:52:16 +0900

    First sample project and todo will be auto-created when user creates new account

--- app/controllers/registrations_modified_controller.rb
+++ app/controllers/registrations_modified_controller.rb
@@ -1,4 +1,14 @@
 class RegistrationsModifiedController < DeviseTokenAuth::RegistrationsController
+
+  def create
+    super do |user|
+      project = user.projects.new(name: 'First Project (Sample)', description: 'describe your project')
+      project.save
+      todo = project.todos.new(task: 'your first wish', user: user)
+      todo.save
+    end
+  end
+
   private
   def sign_up_params
     params.permit(:name, :email, :password)
